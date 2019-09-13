ce2a4003e591677f0208f1b4c61c2a050262a693
antfats <antfats@hotmail.com>
2019-09-12 20:29:03 -0700

    trying to get a sticky bar to work

---
+++ PatriotPools/js/main.js
@@ -0,0 +1,16 @@
+$('document').ready(function () {
+
+    window.onscroll = function () { stickyNav() };
+
+    var navbar = $("#menu");
+    var sticky = navbar.offsetTop;
+
+
+    function stickyNav() {
+        if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+});
